f02c5b74f89a05e5a265b65df8d917c3221d8a6c
hjenryin <72179072+hjenryin@users.noreply.github.com>
2025-02-03 01:27:33 +0800

    bat提示

--- assets/retrieveData.bat
+++ assets/retrieveData.bat
@@ -4,15 +4,14 @@
 
 @REM When adding lines, remember to update the README file! Two additional lines need to be added.
 @REM This file should be encoded in GB2312.
-echo 修改配置、了解更多请至https://github.com/hjenryin/BCJH-Metropolis/ 以便在线运行
 echo 有疑问请至https://github.com/hjenryin/BCJH-Metropolis/discussions/categories/q-a 提问
 echo 有任何想法可至https://github.com/hjenryin/BCJH-Metropolis/discussions/categories/general 讨论
-echo 现已更新汤圆、乌龙等奇奇怪怪的技能，如果发现有问题，欢迎到 https://github.com/hjenryin/BCJH-Metropolis/issues/new 提出~
+echo 如果发现有问题，欢迎到 https://github.com/hjenryin/BCJH-Metropolis/issues/new 提出~
 
 powershell -command "$file = 'ruleData.json'; $halfHourAgo = (Get-Date).AddMinutes(-30); $modifiedTime = (Get-Item $file).LastWriteTime; $modifiedTime -lt $halfHourAgo" | findstr "True" >NUL && call:download_data || echo 半小时内已更新过规则，跳过下载
 
 set "bcjhid="
-set /p bcjhid=如果需要重新下载用户配置，请输入白菜菊花id；否则直接回车：
+set /p bcjhid=如果需要重新下载用户配置，请输入游戏内六位校验码（左上角头像->设置->白菜菊花，注意不是白采菊花内上传的数据ID）；否则直接回车：
 
 if "%bcjhid%"=="" (
     echo 未输入id，跳过下载用户配置
@@ -20,16 +19,15 @@ if "%bcjhid%"=="" (
 
 if not "%bcjhid%"=="" (
     echo 正在下载用户配置
-    powershell -command curl -o "userData.json" "https://bcjh.xyz/api/download_data?id=%bcjhid%"
-    powershell -command $json=$(iwr -uri "https://bcjh.xyz/api/download_data?id=%bcjhid%" ^| ConvertFrom-Json^) ^; if ($json.result -eq 'True'^) { Write-Host 下载成功 -ForegroundColor:Green } else { Write-Host 下载失败，请检查id是否正确 -BackgroundColor:Red -ForegroundColor:White ^}
-    win-iconv\iconv.exe -f utf-8 -t gbk userData.json > tmp 2>NUL
+    @REM powershell -command curl -o "userData.json" "https://bcjh.xyz/api/download_data?id=%bcjhid%"
+    powershell -command $json=$(iwr -uri "https://yx518.com/api/archive.do?token=%bcjhid%" ^| ConvertFrom-Json^) ^; if ($json.ret -eq 'S'^) { Write-Host 下载成功 -ForegroundColor:Green } else { Write-Host 下载失败，请检查id是否正确 -BackgroundColor:Red -ForegroundColor:White ^}
+    win-iconv\iconv.exe -f utf-8 -t gbk directUserData.json > tmp 2>NUL
     if %ERRORLEVEL% EQU 0 (
         del userData.json
         ren tmp userData.json
     )
 )
 echo=
-powershell -command "Write-Host '查看厨具修改教程，请参考' -NoNewline; Write-Host 'https://github.com/hjenryin/BCJH-Metropolis#配置厨具' -ForegroundColor:Blue -NoNewline; Write-Host '。'"
 
 @REM ******************************************************
 @REM ** 如有需要，可以在此修改-C，-R后的数字来更改迭代次数。 **
